e2c593aef651d933cb52e3491230476c0cc00787
Adrian Golian <adix9512@gmail.com>
2020-05-17 01:09:00 +0200

    Add get_probability_vector with dummy values

--- shared/probabilities/handler.py
+++ shared/probabilities/handler.py
@@ -1,3 +1,4 @@
+import numpy as np
 from shared.probabilities import probability_table as pt
 
 
@@ -7,3 +8,26 @@ class Handler(object):
     def __init__(self):
         super(Handler, self).__init__()
         self.probs_table = pt.get()
+
+    def get_probability_vector(self, cards=None, others_card_num=0):
+        if cards is None:
+            raise TypeError("No 'cards' provided.")
+        if not all(isinstance(x, int) for x in cards):
+            raise TypeError("'cards' is not an iterable of integers")
+        self.cards = cards
+
+        if others_card_num <= 0 or others_card_num >= 24:
+            raise ValueError("No valid 'others_card_num' provided")
+        self.others_card_num = others_card_num
+
+        return np.fromfunction(np.vectorize(self.get_bet_prob), (88,))
+
+    def get_bet_prob(self, action_id):
+        if action_id in range(6):
+            if action_id in self.cards:
+                return 1.0
+            else:
+                index = len(self.cards)*100 + self.others_card_num
+                return self.probs_table.loc[[index], ["highcard"]].values[0]
+        else:
+            return 0.0
