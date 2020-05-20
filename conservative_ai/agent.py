1e07b63f8e6f730558446e16102be4523c5d1fa1
Adrian Golian <adix9512@gmail.com>
2020-05-20 22:03:36 +0200

    Analyse game state

--- conservative_ai/agent.py
+++ conservative_ai/agent.py
@@ -1,3 +1,4 @@
+from time import sleep
 from shared.api import game_manager
 from shared.probabilities import handler
 
@@ -31,4 +32,31 @@ class Agent(object):
         """
             Play the game.
         """
-        pass
+        done = False
+        while not done:
+            sleep(1) # DEBUG / DEV
+            succeeded, game_state = self.game_manager.get_game_state()
+            if not succeeded:
+                print("Can't get the game state.")
+                continue
+            if game_state.get("status") != "Running":
+                if game_state.get("status") != "Finished":
+                    print("Game not yet started")
+                else:
+                    print("Game finished")
+                    break
+                continue
+            if game_state.get("cp_nickname") != self.nickname:
+                print("It's not my turn")
+                continue
+            matching_hands = [hand for hand in game_state.get("hands", []) if hand.get("nickname") == self.nickname]
+            if len(matching_hands) != 1:
+                print("Can't get my hand from the game state.")
+                continue
+            hand = [(card["value"], card["colour"]) for card in matching_hands[0]["hand"]]
+            players = game_state.get("players", [])
+            others_card_num = sum([player.get("n_cards") for player in players if player.get("n_cards", self.nickname) != self.nickname])
+            if not players or not others_card_num:
+                print("Can't get my hand or other player's card numbers from the game state.")
+                continue
+            print("Here's my move")
